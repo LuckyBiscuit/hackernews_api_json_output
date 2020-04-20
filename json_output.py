f786719aaac6b07fae6ebf2d83b9d4da281362dc
Lucky <oliverewing14@gmail.com>
2020-04-20 20:52:02 +0100

    First_commit

--- json_output.py
+++ json_output.py
@@ -3,3 +3,8 @@ import requests
 import pprint
 import sys
 
+# request for the hackernews api which lists the ID's for each post but no data from those ids
+r = requests.get ('https://hacker-news.firebaseio.com/v0/topstories.json')
+packages_json = r.json()
+
+print(packages_json)
