ab7bdb578e381c1de64d4dbeb040b1da3417f0d0
Lucky <oliverewing14@gmail.com>
2020-04-20 21:03:27 +0100

    check for response from package_url

--- json_output.py
+++ json_output.py
@@ -7,4 +7,12 @@ import sys
 r = requests.get ('https://hacker-news.firebaseio.com/v0/topstories.json')
 packages_json = r.json()
 
-print(packages_json)
+# print(packages_json)
+
+# article_id variable to be run through the packages.json to pass through the ids with the different api call for each individual item.
+for num, article_id in enumerate(packages_json, start=1):
+    package_url = f'https://hacker-news.firebaseio.com/v0/item/{article_id}.json'
+    r = requests.get(package_url)
+    packages_json = r.json()
+
+    print(package_url)
